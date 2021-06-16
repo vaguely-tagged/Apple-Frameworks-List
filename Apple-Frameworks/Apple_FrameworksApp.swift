13d9120260b96ee704315ea9904bfaae6a256fb5
Vincent Sgherzi <sgherzivincent@gmail.com>
2021-06-16 17:41:10 +0900

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Vince Sgherzi on 6/16/21.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
